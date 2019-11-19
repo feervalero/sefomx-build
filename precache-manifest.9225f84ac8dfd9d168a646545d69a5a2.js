fb2ea0c2d3c359b863d873ea0bd174b83691c467
Fernando Valero <fernandovalero@MacBook-Air-de-Fernando.local>
2019-11-19 09:19:22 -0600

    release 1.1

---
+++ precache-manifest.9225f84ac8dfd9d168a646545d69a5a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2aec57ef0a204758bb4970fa40847e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997d6e5cf84fe2282a48",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "110f39863d9914576886",
+    "url": "/static/js/2.f82a4d95.chunk.js"
+  },
+  {
+    "revision": "997d6e5cf84fe2282a48",
+    "url": "/static/js/main.fa0bed61.chunk.js"
+  },
+  {
+    "revision": "39d19c32d500d6d32874",
+    "url": "/static/js/runtime-main.6ea226b5.js"
+  }
+]);
